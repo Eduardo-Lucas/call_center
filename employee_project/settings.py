6ca3163bbb7c9d7689587b9a6af89d22ea9f29d2
Eduardo Lucas <eduardolucas40@gmail.com>
2020-05-02 21:39:37 -0300

    Acertando o statifiles

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
