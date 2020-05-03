a4546b59f75d448fc82328dcca7c6dccc1b36383
Eduardo Lucas <eduardolucas40@gmail.com>
2020-05-02 21:36:08 -0300

    Colocando o nome do usuário na NavBar

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#l55&jyvqu$kukw^*4h0s18r+f)2s_gm(3i-7=@7*1#nr1q4yn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ba-call-center.herokuapp.com', '127.0.0.1']
 
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
